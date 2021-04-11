2f0dc92762b1e2c85f44cf8612da21d77a2e13a1
Joshua May <notjosh@gmail.com>
2021-04-11 20:00:29 +0200

    I _think_ this can be built out of the box now

--- packages/highlighter/test/index.test.ts
+++ packages/highlighter/test/index.test.ts
@@ -1,34 +1,31 @@
 import * as rangy from "@rangy/core";
-import "@rangy/classapplier";
-import "@rangy/highlighter";
-import {createRangeInHtml} from "@rangy/test-util/testutils";
+import * as classapplier from "@rangy/classapplier";
+import * as highlighter from "@rangy/highlighter";
+import { createRangeInHtml } from "@rangy/test-util/testutils";
 import "@rangy/test-util/qunit-ex";
 
 QUnit.module("Highlighter module tests");
 
-    QUnit.test("highlightSelection test", function(t) {
-        var applier = rangy.createClassApplier("c1");
-        var highlighter = rangy.createHighlighter();
-        highlighter.addClassApplier(applier);
+QUnit.test("highlightSelection test", function (t) {
+  var applier = classapplier.createClassApplier("c1");
+  var highlighter = highlighter.createHighlighter();
+  highlighter.addClassApplier(applier);
 
-        var testEl = document.getElementById("test");
-        var range = createRangeInHtml(testEl, 'one [two] three four');
-        range.select();
+  var testEl = document.getElementById("test");
+  var range = createRangeInHtml(testEl, "one [two] three four");
+  range.select();
 
-        var highlights = highlighter.highlightSelection("c1");
-        
-        t.equal(highlights.length, 1);
-        
-        
-        //t.assertEquals(highlights.length, 1);
+  var highlights = highlighter.highlightSelection("c1");
 
+  t.equal(highlights.length, 1);
 
-    });
+  //t.assertEquals(highlights.length, 1);
+});
 
-    QUnit.test("Options test (issue 249)", function(t) {
-        var applier = rangy.createClassApplier("c1");
-        var highlighter = rangy.createHighlighter();
-        highlighter.addClassApplier(applier);
+QUnit.test("Options test (issue 249)", function (t) {
+  var applier = classapplier.createClassApplier("c1");
+  var highlighter = highlighter.createHighlighter();
+  highlighter.addClassApplier(applier);
 
-        highlighter.highlightSelection("c1", { selection: rangy.getSelection() });
-    });
+  highlighter.highlightSelection("c1", { selection: rangy.getSelection() });
+});
--- packages/selectionsaverestore/test/index.test.ts
+++ packages/selectionsaverestore/test/index.test.ts
@@ -1,22 +1,25 @@
 import * as rangy from "@rangy/core";
 import "@rangy/test-util/qunit-ex";
-import "@rangy/selectionsaverestore";
+import * as selectionsaverestore from "@rangy/selectionsaverestore";
 
 QUnit.module("Selection save/restore module tests");
-QUnit.test("Issue 140 (saveSelection reverses backward selection)", function(t) {
-        var testEl = document.getElementById("qunit-fixture");
-        testEl.innerHTML = "test";
-        var range = rangy.createRange();
-        range.setStartAndEnd(testEl.firstChild, 1, 3);
-        var sel = rangy.getSelection();
-        sel.addRange(range, "backward");
+QUnit.test(
+  "Issue 140 (saveSelection reverses backward selection)",
+  function (t) {
+    var testEl = document.getElementById("qunit-fixture");
+    testEl.innerHTML = "test";
+    var range = rangy.createRange();
+    range.setStartAndEnd(testEl.firstChild, 1, 3);
+    var sel = rangy.getSelection();
+    sel.addRange(range, "backward");
 
-        t.ok(sel.isBackward());
-        t.equal(sel.rangeCount, 1);
-        t.ok(sel.getRangeAt(0).equals(range));
+    t.ok(sel.isBackward());
+    t.equal(sel.rangeCount, 1);
+    t.ok(sel.getRangeAt(0).equals(range));
 
-        rangy.saveSelection();
+    selectionsaverestore.saveSelection();
 
-        t.ok(sel.isBackward());
-        t.equal(sel.rangeCount, 1);
-    });
+    t.ok(sel.isBackward());
+    t.equal(sel.rangeCount, 1);
+  }
+);
