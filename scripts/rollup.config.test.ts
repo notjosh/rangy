eace92d8d1d0dcb98ef556dc94449059790c8682
Joshua May <notjosh@gmail.com>
2021-04-11 21:23:10 +0200

    Bump dependencies

--- scripts/rollup.config.test.ts
+++ scripts/rollup.config.test.ts
@@ -3,58 +3,52 @@
  * The iife file is then used to verify that rangy2 & rangy modules (ie, rangy-classapplier)
  * can be used by referencing directly in `script` tag in html files
  */
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import * as glob from 'glob';
-import {packages, packagesDir} from "./util";
-import {resolve, join} from "path";
-import {ModuleFormat, RollupOptions} from "rollup";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import fs from "fs";
-import sourceMaps from 'rollup-plugin-sourcemaps'
+import * as glob from "glob";
+import { join, resolve } from "path";
+import { ModuleFormat, Plugin, RollupOptions } from "rollup";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import { packages, packagesDir } from "./util";
 
-const plugins = [
-    nodeResolve(),
-    commonjs(),
-    sourceMaps(),
-];
+const plugins: Plugin[] = [nodeResolve(), commonjs(), sourceMaps()];
 
 //all rangy modules are external dependencies to test code
-const external = packages.map(n => '@rangy/' + n);
+const external = packages.map((n) => "@rangy/" + n);
 
 //map all external dependencies to global name 'rangy'
 const globals = {};
-external.forEach(n => Object.assign(globals, {[n]: 'rangy'}));
+external.forEach((n) => Object.assign(globals, { [n]: "rangy" }));
 
 function configsFor(module: string) {
-    const cwd = resolve(packagesDir, module, 'test');
-    const tsconfig = resolve(cwd, 'tsconfig.json');
+  const cwd = resolve(packagesDir, module, "test");
+  const tsconfig = resolve(cwd, "tsconfig.json");
 
-    if (!fs.existsSync(tsconfig)) return [];
+  if (!fs.existsSync(tsconfig)) return [];
 
-    function config(f: string, format: ModuleFormat = 'iife'): RollupOptions {
-        f = join(cwd, f);
-        return {
-            input: [f],
-            output: {
-                format,
-                file: f.replace(/\.js$/, `.${format}.js`),
-                name: 'unnamed',
-                globals,
-                sourcemap: true
-            },
-            external,
-            plugins,
-        }
-    }
-    const configs = glob
-        .sync('**/*.test.js', {cwd})
-        .map(f => config(f));
+  function config(f: string, format: ModuleFormat = "iife"): RollupOptions {
+    f = join(cwd, f);
+    return {
+      input: [f],
+      output: {
+        format,
+        file: f.replace(/\.js$/, `.${format}.js`),
+        name: "unnamed",
+        globals,
+        sourcemap: true,
+      },
+      external,
+      plugins,
+    };
+  }
+  const configs = glob.sync("**/*.test.js", { cwd }).map((f) => config(f));
 
-    if (module == 'classapplier') {
-        // push amdTestExampleConfig
-        configs.push(config("index.test.js", 'amd'));
-    }
-    return configs;
+  if (module == "classapplier") {
+    // push amdTestExampleConfig
+    configs.push(config("index.test.js", "amd"));
+  }
+  return configs;
 }
 
 export default packages.flatMap(configsFor);
--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -1,88 +1,96 @@
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import * as lerna from '../lerna.json';
-import replace from 'rollup-plugin-re';
-import {RollupOptions} from "rollup";
-import {packages, packagesDir} from "./util";
-import {resolve} from "path";
-import { uglify } from 'rollup-plugin-uglify'
-import { terser } from 'rollup-plugin-terser'
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import { resolve } from "path";
+import { Plugin, RollupOptions } from "rollup";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import { terser } from "rollup-plugin-terser";
+import * as lerna from "../lerna.json";
+import { packages, packagesDir } from "./util";
 
-const buildVars = (function() {
-    const date = new Date();
-    const month = "January,February,March,April,May,June,July,August,September,October,November,December"
-        .split(",")[date.getMonth()];
-    return {
-        "%%build:version%%": lerna.version,
-        "%%build:date%%": date.getDate() + " " + month + " " + date.getFullYear(),
-        "%%build:year%%,": date.getFullYear() + ','
-    };
+const buildVars = (() => {
+  const date = new Date();
+  const month = "January,February,March,April,May,June,July,August,September,October,November,December".split(
+    ","
+  )[date.getMonth()];
+  return {
+    "%%build:version%%": lerna.version,
+    "%%build:date%%": date.getDate() + " " + month + " " + date.getFullYear(),
+    "%%build:year%%,": date.getFullYear() + ",",
+  };
 })();
 
-const plugins = [
-    replace({
-        exclude: 'node_modules/**',
-        replaces: buildVars,
-        patterns: [ //remove logging
-            {
-                test: /(.*log4javascript.*)|(\s*(\/\/\s*)?log\.(trace|debug|info|warn|error|fatal|time|timeEnd|group|groupEnd).+)/g,
-                replace: '',
-            }
-        ]
-    }),
-    nodeResolve(),
-    commonjs(),
-    // Resolve source maps to the original source
-    sourceMaps(),
+const plugins: Plugin[] = [
+  replace({
+    exclude: "node_modules/**",
+    values: buildVars,
+    // TODO: strip log4javascript (lost this when upgrading @rollup/plugin-replace)
+    // patterns: [
+    //   //remove logging
+    //   {
+    //     test: /(.*log4javascript.*)|(\s*(\/\/\s*)?log\.(trace|debug|info|warn|error|fatal|time|timeEnd|group|groupEnd).+)/g,
+
+    //     replace: "",
+    //   },
+    // ],
+  }),
+  nodeResolve(),
+  commonjs(),
+  // Resolve source maps to the original source
+  sourceMaps(),
+  terser(),
 ];
 
-function concatIf<T>(test: boolean, a: T[], b:()=>T[]) {
-    return test? a.concat(b()) : a;
+function concatIf<T>(test: boolean, a: T[], b: () => T[]) {
+  return test ? a.concat(b()) : a;
 }
 
 function outputFile(f: string, isProd: boolean) {
-    return isProd ? f.replace(/\.js$/, '.min.js') : f;
+  return isProd ? f.replace(/\.js$/, ".min.js") : f;
 }
 
 function configsFor(module: string, isProd: boolean): RollupOptions[] {
-    // const tsconfig = await import(`${srcDir}/${name}/tsconfig.json`);
-    // const outDir = tsconfig.compilerOptions.outDir;
-    const d = resolve(packagesDir, module, 'lib');
-    const isRangyModule = module !== 'core';
-    // Indicate here external modules you don't wanna include in your bundle
-    const external = isRangyModule? ['@rangy/core'] : [];
-    return [
-        {   input: [`${d}/esm5/index.js`],
-            output: {
-                file: outputFile(`${d}/bundles/index.umd.js`, isProd),
-                format: 'umd',
-                name: 'rangy',
-                sourcemap: true,
-                extend: isRangyModule,
-                globals: { "@rangy/core": "rangy" }
-            },
-            inlineDynamicImports: true,
-            external,
-            plugins: concatIf(isProd, plugins, uglify),
-        },
-        {   input: [`${d}/esm2015/index.js`],
-            output: {
-                file: outputFile(`${d}/bundles/index.esm.js`, isProd),
-                format: 'es',
-                sourcemap: true,
-            },
-            inlineDynamicImports: true,
-            // only bundle tslib, core-js in umd file
-            external: (id) => [...external, 'tslib', 'core-js'].includes(id) || id.startsWith('core-js/'),
-            plugins: concatIf(isProd, plugins, terser),
-        }
-    ]
+  // const tsconfig = await import(`${srcDir}/${name}/tsconfig.json`);
+  // const outDir = tsconfig.compilerOptions.outDir;
+  const d = resolve(packagesDir, module, "lib");
+  const isRangyModule = module !== "core";
+  // Indicate here external modules you don't wanna include in your bundle
+  const external = isRangyModule ? ["@rangy/core"] : [];
+  return [
+    {
+      input: [`${d}/esm5/index.js`],
+      output: {
+        file: outputFile(`${d}/bundles/index.umd.js`, isProd),
+        format: "umd",
+        name: "rangy",
+        sourcemap: true,
+        extend: isRangyModule,
+        globals: { "@rangy/core": "rangy" },
+      },
+      inlineDynamicImports: true,
+      external,
+      plugins: concatIf(isProd, plugins, () => [terser()]),
+    },
+    {
+      input: [`${d}/esm2015/index.js`],
+      output: {
+        file: outputFile(`${d}/bundles/index.esm.js`, isProd),
+        format: "es",
+        sourcemap: true,
+      },
+      inlineDynamicImports: true,
+      // only bundle tslib, core-js in umd file
+      external: (id) =>
+        [...external, "tslib", "core-js"].includes(id) ||
+        id.startsWith("core-js/"),
+      plugins: concatIf(isProd, plugins, () => [terser()]),
+    },
+  ];
 }
 
-const configs = packages.flatMap(m => [
-    ...configsFor(m, false),
-    ...configsFor(m, true)
+const configs = packages.flatMap((m) => [
+  ...configsFor(m, false),
+  ...configsFor(m, true),
 ]);
 
 export default configs;
