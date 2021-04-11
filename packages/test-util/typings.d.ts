9e0562254027215eb8adfb3eb9075c6447e9629f
Joshua May <notjosh@gmail.com>
2021-04-11 18:49:58 +0200

    Fixed building, mostly, sort of

--- packages/test-util/typings.d.ts
+++ packages/test-util/typings.d.ts
@@ -1,30 +1,34 @@
 import "qunit";
 
 declare global {
-    interface Assert {
-        notThrows(f: () => void, msg?: string): void;
-        assertNull(state: any, message?: string): void;
-        assertFalse(state: any, message?: string): void;
-        assertTrue(state: any, message?: string): void;
-    }
-    interface QUnit {
-        testEx(name: string,
-             callback: (assert: Assert) => void,
-             setUp: (assert: Assert) => void,
-             tearDown: (assert: Assert) => void): void;
-        skipEx(name: string,
-               callback: (assert: Assert) => void,
-               setUp: (assert: Assert) => void,
-               tearDown: (assert: Assert) => void): void;
-    }
+  interface Assert {
+    notThrows(f: () => void, msg?: string): void;
+    assertNull(state: any, message?: string): void;
+    assertFalse(state: any, message?: string): void;
+    assertTrue(state: any, message?: string): void;
+  }
+  interface QUnit {
+    testEx(
+      name: string,
+      callback: (assert: Assert) => void,
+      setUp: (assert: Assert) => void,
+      tearDown: (assert: Assert) => void
+    ): void;
+    skipEx(
+      name: string,
+      callback: (assert: Assert) => void,
+      setUp: (assert: Assert) => void,
+      tearDown: (assert: Assert) => void
+    ): void;
+  }
 }
 
 declare module "bowser" {
-    namespace Parser {
-        interface Parser {
-            isEngine(engineName: string): boolean;
-        }
+  namespace Parser {
+    interface Parser {
+      isEngine(engineName: string): boolean;
     }
+  }
 }
 
 export * from "./index";
